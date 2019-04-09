87250093f6efd020543de13eafc09a16dfc8c568
Henry Koehler <henryk1229@gmail.com>
2019-04-09 15:57:03 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,6 +5,7 @@ def key_for_min_value(name_hash)
   if name_hash.length == 0 
     nil 
   else 
-    winner = ""
+    min_value = nil
+    min_key = nil
     name_hash.each do |key, value|
-      if 
+      if min_value == nil 
