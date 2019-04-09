6056d3046de078f55be29f9a7a672f7f4e1104ba
Henry Koehler <henryk1229@gmail.com>
2019-04-09 15:23:08 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,16 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  if name_hash.length == 0 
+    nil 
+  else 
+    winner = ""
+    until winner != "" do 
+      name_hash.collect do |key, value|
+        i = 0 
+        value > i ? winner = [:key] : i += 1 
+      end
+    end
+  end
+  winner
 end
